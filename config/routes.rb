aa5dd3424140716ead7b8bb780fbc49bef9b3ff1
latttchc <iam.yuk2882@gmail.com>
2025-06-09 00:10:25 +0900

    feat:Rails routeをv1用に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace "v1", defaults: { format: :json } do
+    resources :users
+  end
 end
